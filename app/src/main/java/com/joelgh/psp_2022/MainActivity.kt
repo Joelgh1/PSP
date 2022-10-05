1a294aaa7580431121268b57457e617c084e509f
Joel Garrido <joelgamar@gmail.com>
2022-10-05 19:01:55 +0200

    Ut02Ex01 Retrofit practise with internet permission

--- app/src/main/java/com/joelgh/psp_2022/MainActivity.kt
+++ app/src/main/java/com/joelgh/psp_2022/MainActivity.kt
@@ -2,10 +2,20 @@ package com.joelgh.psp_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.joelgh.psp_2022.ut02.ex01.data.remote.RetrofitApiClient
 
 class MainActivity : AppCompatActivity() {
+
+    val apiClient = RetrofitApiClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread{
+            val users = apiClient.getUsers()
+            Log.d("@dev", "Usuarios: $users")
+        }.start()
     }
 }
